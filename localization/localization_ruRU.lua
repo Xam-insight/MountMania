b542bc5ddc8b32c41a9ea51015adbcf4956b4a6e
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-02-03 09:00:46 +0300

    Create localization_ruRU.lua
    Translation from ZamestoTV

---
+++ localization/localization_ruRU.lua
@@ -0,0 +1,31 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("MountMania", "ruRU", true)
+
+if L then
+
+L["SPACE_BEFORE_DOT"] = ""
+
+L["MOUNTMANIA_WELCOME"] = "Введите /mnt для переключения Ездомании."
+
+L["MOUNTMANIA_MOUNTSUMMONER"] = "Вызов средства передвижение"
+L["MOUNTMANIA_MOUNTSUMMONER_TOOLTIP"] = "Призовите случайное средство передвижение, чтобы бросить вызов|nдругим игрокам в вашей группе."
+
+L["MOUNTMANIA_ENDER"] = "Конец игры"
+L["MOUNTMANIA_ENDER_TOOLTIP"] = "Завершите игру и объявите победителя(ей)."
+
+L["LOCKBUTTON_TOOLTIP"] = "Замок"
+L["LOCKBUTTON_TOOLTIPDETAIL"] = "Блокирует/разблокирует окно."
+
+L["MOUNTMANIA_CATCH_GETREADY"] = "Приготовьтесь к Ездомании! Скорее призовите того же средство передвижения, что и я, чтобы заработать очки!"
+L["MOUNTMANIA_CATCH_END"] = "Возвращайтесь позже на еще один раунд Ездомании!"
+L["MOUNTMANIA_CATCH_WINNER"] = "Посмотрим, кто победил!"
+L["MOUNTMANIA_CATCH_NEXT1"] = "Приготовьтесь к следующему средству передвижения!"
+L["MOUNTMANIA_CATCH_NEXT2"] = "Далее у нас..."
+L["MOUNTMANIA_CATCH_MOUNT"] = "%s!"
+
+L["ENABLE_MOUNTMANIA"] = "Переключить Ездоманию"
+L["ENABLE_MOUNTMANIA_DESC"] = "Показать/скрыть окно Ездомании"
+
+L["MOUNTMANIA_OPTIONS_ALPHA"] = "Ездомания альфа"
+L["MOUNTMANIA_OPTIONS_ALPHA_DESC"] = "Определите альфу Ездомании"
+
+end
